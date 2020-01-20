0830d57bb8f9d682590fe07a127d4a9f35705567
BelidaVikas09 <59680462+BelidaVikas09@users.noreply.github.com>
2020-01-20 23:57:22 +0530

    Update JDBC.java

--- 17-01-2020_AN_AdvanceJava/src/jdbc/JDBC.java
+++ 17-01-2020_AN_AdvanceJava/src/jdbc/JDBC.java
@@ -1,8 +1,26 @@
 package jdbc;
-import java.sql.Connection;
+import java.util.ArrayList;
+import java.sql.*;
 public class JDBC {
-  public static void main(String args[]) {
-	 System.out.println("this is not cool!");
-  }
+	public static void main(String args[]){
+		ArrayList<EmployeeDept>arr=new ArrayList<>();
+		try{  
+		Class.forName("com.mysql.jdbc.Driver");
+                String url="jdbc:mysql://localhost:3306/employee";
+                String user="root";
+                String password="viaks@1262";  
+		Connection con=DriverManager.getConnection(url,user,password);  
+		String query="{CALL join_procedure()}";
+		CallableStatement stmt=con.prepareCall(query);  
+		ResultSet rs=stmt.executeQuery();  
+		while(rs.next()) {
+			EmployeeDept ed=new EmployeeDepartment(rs.getInt("emp_id"),rs.getString("emp_name"),rs.getString("Designation"),rs.getString("salary"),rs.getInt("dep_id"),rs.getString("dep_name"));
+			arr.add(ed);
+		} 
+		for(EmployeeDept e:arr) {
+			System.out.println(e);
+		}
+		con.close();  
+		}catch(Exception e){ System.out.println(e.toString());}  
+		}
 }
- 
